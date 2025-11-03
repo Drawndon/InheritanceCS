1c298d540f4a9cb97a85671eee299a5e86634c0e
Андрей Полищук <dcnfandrey@mail.ru>
2025-11-03 19:33:46 +0300

    Add project files.

---
+++ Academy/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Academy");
+			Human human = new Human("Montana", "Antonio", 25);
+			human.Info();
+		}
+	}
+}
