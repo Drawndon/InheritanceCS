6de3fa5cbd9ef6e1a3cb12e275ef56363c511100
Андрей Полищук <dcnfandrey@mail.ru>
2025-11-06 21:21:45 +0300

    Abstract base class

---
+++ AbstractBaseClass/Vehicle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractBaseClass
+{
+	internal abstract class Vehicle
+	{
+		int speed;
+		public abstract void Move();
+	}
+}
