f8e0898ae111c7df0a71e4264fc0c5050167e494
Андрей Полищук <dcnfandrey@mail.ru>
2025-11-07 18:59:36 +0300

    Square & Circle CHECK DONE

--- AbstractBaseClass/Program.cs
+++ AbstractBaseClass/Program.cs
@@ -8,11 +8,20 @@ namespace AbstractBaseClass
 {
 	internal class Program
 	{
+		static readonly string delimeter = "\n---------------------------------------------------------------\n";
 		static void Main(string[] args)
 		{
 			Rectangle rect = new Rectangle(100, 200, 50, 50, 2);
 			rect.Info();
+			Console.WriteLine(delimeter);
 
+			Square sq = new Square(100, 50, 50, 3);
+			sq.Info();
+			Console.WriteLine(delimeter);
+
+			Circle circ = new Circle(10, 150, 150, 5);
+			circ.Info();
+			Console.WriteLine(delimeter);
 
 		}
 	}
